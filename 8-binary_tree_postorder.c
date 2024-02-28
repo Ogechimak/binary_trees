e3c08bdee83a403e586299d34ae728bb76ec7a26
Ogechimak <ogechimac111@gmail.com>
2024-02-28 10:29:05 +0100

    post_order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -8,12 +8,12 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (tree == NULL || func == NULL)
-        {
-        return;
-        }
-                binary_tree_postorder(tree->left, func);
-                func(tree->n);
-                binary_tree_postorder(tree->right, func);
+	if (tree == NULL || func == NULL)
+	{
+	return;
+	}
+	binary_tree_postorder(tree->left, func);
+	func(tree->n);
+	binary_tree_postorder(tree->right, func);
 
 }
